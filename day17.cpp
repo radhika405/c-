2a10538f60d9f1cccc3cc2399093b72cf089bf66
radhika405 <vermaradhika145@gmail.com>
2024-07-16 20:28:12 +0530

    Add files via upload

---
+++ day17.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<list>
+using namespace std;
+int main()
+{
+    list<int>l;
+    l.push_back(10);
+    l.push_front(12);
+    for(int i:l){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    l.erase(l.begin());
+    cout<<"after ersae"<<endl;
+    for(int i:l){
+        cout<<i<<" ";
+        cout<<" size of list"<<l.size()<<endl;
+    }
+    
+}
