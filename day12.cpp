af0c3fb9522ac705009287ddb15ff14530aa772a
radhika405 <vermaradhika145@gmail.com>
2024-07-16 20:25:29 +0530

    Add files via upload

---
+++ day12.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+int main(){
+    std::string str1 ="hell0,";
+    std::string str2 ="world!";
+    std::string result = str1+str2;
+    std::cout<<"concatenated string:"<<result<<std::endl;
+    return 0;
+
+}
