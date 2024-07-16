b5ee55489163e81982d733e56f0767a38760fefa
radhika405 <vermaradhika145@gmail.com>
2024-07-16 20:23:50 +0530

    Add files via upload

---
+++ day9.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int classes_held, classes_attendend;
+    cout<<"enter the number of classes held:";
+    cin>>classes_held;
+    cout<<"enter the number of classes attendend:";
+    cin>>classes_attendend;
+    
+}
